1110b3ef69b8781a4a02a5b408ecd3128ea6424d
Алиса <alisia.bajeeva247@gmail.com>
2026-06-07 12:36:43 +0500

    kk

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://urfu-things-1.onrender.com", "https://alisaboules.github.io", "http://localhost:5176"],
+    allow_origins=["https://urfu-things-1.onrender.com", "https://alisaboules.github.io", "http://localhost:5176", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
